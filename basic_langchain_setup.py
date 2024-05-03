11a979c3c1fbcd2f63091eef24c9cb443d1aaf75
fzorrilla-ml <66095840+fzorrilla-ml@users.noreply.github.com>
2024-05-03 17:14:32 -0400

    Update basic_langchain_setup.py

--- basic_langchain_setup.py
+++ basic_langchain_setup.py
@@ -1,23 +1,30 @@
-from langchain import PromptTemplate, LLMChain
-from langchain.llms import GPT4All
-from langchain.callbacks.base import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-local_path = './models/gpt4all-converted.bin' 
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+from langchain.chains import LLMChain
+from langchain_community.llms import GPT4All
+from langchain_core.prompts import PromptTemplate
 
 template = """Question: {question}
 
-Answer: Let's think step by step.
+Answer: Let's think step by step."""
 
-"""
+prompt = PromptTemplate.from_template(template)
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
-llm = GPT4All(model=local_path, callback_manager=callback_manager, verbose=True)
+local_path = (
+    "./models/Meta-Llama-3-8B-Instruct.Q4_0.gguf"  # replace with your desired local file path
+)
+
+# Callbacks support token-wise streaming
+callbacks = [StreamingStdOutCallbackHandler()]
+
+# Verbose is required to pass to the callback manager
+llm = GPT4All(model=local_path, callbacks=callbacks, verbose=True)
+
+# If you want to use a custom model add the backend parameter
+# Check https://docs.gpt4all.io/gpt4all_python.html for supported backends
+llm = GPT4All(model=local_path, backend="gptj", callbacks=callbacks, verbose=True, device='gpu')
+
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-# question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
-
-question = input("Enter your question: ")
+question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
 
 llm_chain.run(question)
